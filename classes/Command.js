8b37a69c9ae8891ab613dfb7122e524649fbfc64
IsoEntity <xynfamail@gmail.com>
2018-03-28 15:37:14 +0200

    promises Parser.js

---
+++ classes/Command.js
@@ -0,0 +1,18 @@
+// community modules
+const Promise = require("promise");
+
+// imports
+const methods = require("../methods.js");
+
+class Command {
+
+    parse(input) {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+
+}
+
+// exports
+module.exports = Command;
---
+++ classes/Option.js
@@ -0,0 +1,18 @@
+// community modules
+const Promise = require("promise");
+
+// imports
+const methods = require("../methods.js");
+
+class Option {
+
+    parse(input, parent) {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+
+}
+
+// exports
+module.exports = Option;
