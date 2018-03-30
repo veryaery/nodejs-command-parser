795e049e8b95a7cb8925448ab60fbdbe34291526
IsoEntity <xynfamail@gmail.com>
2018-03-30 20:22:16 +0200

    export methods.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Fault = require("./classes/Fault.js");
 const Option = require("./classes/Option.js");
 const Parser = require("./classes/Parser.js");
 
+const methods = require("./methods.js");
+
 const types = {
     Either: require("./types/Either.js"),
     List: require("./types/List.js"),
@@ -19,4 +21,5 @@ exports.Command = Command;
 exports.Fault = Fault;
 exports.Option = Option;
 exports.Parser = Parser;
+exports.methods = methods;
 exports.types = types;
